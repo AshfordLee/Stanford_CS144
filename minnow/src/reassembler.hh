5ed6e6beb1cb46249baacc87a3217488087cafdf
AshfordLee <1207833942@qq.com>
2025-07-06 15:22:00 +0800

    CheckPoint1 Finished

--- minnow/src/reassembler.hh
+++ minnow/src/reassembler.hh
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include<map>
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output ) 
+  : output_( std::move( output ) ),
+    unassembled_segments_(),
+    next_index_( 0 ),
+    bytes_pending_( 0 ),
+    eof_flag_( false ),
+    eof_index_( 0 )
+  {}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -29,6 +37,9 @@ public:
    * The Reassembler should close the stream after writing the last byte.
    */
   void insert( uint64_t first_index, std::string data, bool is_last_substring );
+  void store_segment(uint64_t first_index,std::string &data);
+  void try_assemble();
+  void push_output(std::string &data);
 
   // How many bytes are stored in the Reassembler itself?
   // This function is for testing only; don't add extra state to support it.
@@ -43,4 +54,22 @@ public:
 
 private:
   ByteStream output_;
+
+  std::map<uint64_t,std::string> unassembled_segments_;
+  // 起到缓冲区的作用，保留那些还没被处理到output_流当中的段
+
+  uint64_t next_index_=0;
+  // 下一个期望写入ByteStream的索引是什么？
+
+  uint64_t  bytes_pending_=0;
+  // 时刻记录缓冲区里面的数量
+
+  bool eof_flag_=false;
+  // 记录有没有收到最后那个段
+
+  uint64_t eof_index_=0;
+  // 如果收到了最后的段，那么最后那个段的start_index就记作结束索引
+
+
+
 };
